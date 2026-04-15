752f7a7ee07d57685a7d1e519c6d790d058d351d
amandarosendal <amandarosendal@hotmail.com>
2026-04-15 13:45:56 +0200

    create button that increases

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-console.log("hello")
+// click a button or object to earn points so that i can increase my score
+//see my current score during the game so that i know how well i am doing
+//see a countdown timer so that i know how much time is left. serInterval
+
+
+//variables
+let score= 0;
+
+// HTML DOM
+const button1 = document.getElementById('button1');
+const scoreDisplay = document.getElementById('scoreDisplay');
+
+
+// UI Functions
+button1.addEventListener('click', () => {
+increaseScore();
+})
+
+
+// Functions
+function increaseScore() {
+  score++;
+ scoreDisplay.innerText = score;
+}
