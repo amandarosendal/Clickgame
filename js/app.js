a5d133621c669cda75c7384f14560b2d919e73f5
amandarosendal <amandarosendal@hotmail.com>
2026-04-17 13:34:37 +0200

    clocka som tickar

--- js/app.js
+++ js/app.js
@@ -1,24 +1,37 @@
 // click a button or object to earn points so that i can increase my score
 //see my current score during the game so that i know how well i am doing
+
 //see a countdown timer so that i know how much time is left. serInterval
 
 
 //variables
 let score= 0;
+let timeLeft= 60;
+
 
 // HTML DOM
 const button1 = document.getElementById('button1');
 const scoreDisplay = document.getElementById('scoreDisplay');
+const timerDisplay = document.getElementById('timerDisplay');
 
 
-// UI Functions
+// UI Functions and events
 button1.addEventListener('click', () => {
 increaseScore();
 })
+
+// TODO: Start only when "click me is Clicked"
+setInterval(countdown, 1000);
 
 
 // Functions
 function increaseScore() {
   score++;
  scoreDisplay.innerText = score;
+}
+
+function countdown() {
+  timeLeft--;
+ timerDisplay.innerText = timeLeft;
+ // TODO: Stop timer at the end
 }
